1ef71cb8e76e615a4ec8fd4bcc12d063ee8c2b7b
rougeo <bsdiallo07@gmail.com>
2015-11-25 12:10:56 +0100

    Gestion de Protocole: Fin du test TCP/IP avec telnet juste pour la connexion

--- M1_STRI_TP_Java/src/main/java/TP1_BANQUE/MainServTCP.java
+++ M1_STRI_TP_Java/src/main/java/TP1_BANQUE/MainServTCP.java
@@ -3,6 +3,15 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+/*
+le port et l'adresse IP¨du serveur ne change pas, c'est le port 
+des differents clients qui changent
+il faut synchroniser la HashMap
+*/
+
+/*
+Pour UDP, putty ne marche pas pour les test, il faut creer un client UDP
+*/
 package TP1_BANQUE;
 
 import java.util.*;
