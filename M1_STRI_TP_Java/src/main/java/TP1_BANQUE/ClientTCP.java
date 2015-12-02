5e35c2f6a97d43dd497d660b0bb5b69debf069d5
rougeo <bsdiallo07@gmail.com>
2015-12-02 22:03:42 +0100

    Gestion de Protocole: Fin du test TCP/IP avec telnet juste pour la connexion

--- M1_STRI_TP_Java/src/main/java/TP1_BANQUE/ClientTCP.java
+++ M1_STRI_TP_Java/src/main/java/TP1_BANQUE/ClientTCP.java
@@ -6,10 +6,48 @@
 package TP1_BANQUE;
 
 import java.util.*;
+import java.util.*;
+import java.net.*;
+import java.io.*;
 /**
  *
  * @author BoubacarSidy
  */
 public class ClientTCP {
     
+    public ClientTCP(String NomServeur, int Numport){
+        
+        
+        Socket client = null;
+        PrintStream fluxSortant = null;
+        BufferedReader fluxEntrant = null;
+
+        try {
+            // echoSocket = new Socket("taranis", 7);
+            client = new Socket(NomServeur, Numport);
+            fluxSortant= new PrintStream(client.getOutputStream());
+            fluxEntrant = new BufferedReader(new InputStreamReader(client.getInputStream()));
+        
+          }catch(IOException ioe) { 
+              System.out.println("Erreur de création du Client socket: " + ioe.getMessage()); 
+              return; 
+        }
+    
+    
+ }   
+      
+    public void traitementTCP() throws IOException{
+       
+        String requete;
+        BufferedReader entreeClavier = new BufferedReader(new InputStreamReader(System.in));
+	        
+        while(true){
+            System.out.print ("\n Saisir une requete: ");
+            requete = entreeClavier.readLine();
+	    fluxSortant.println(requete);
+	    System.out.println("REPONSE: \n" + fluxEntrant.readLine());
+           
+	}
+        client.close();
+    }
 }
